231f7b772e56d7a157126ab481f4056ee5585813
Scott Purdy <spurdy@numenta.com>
2016-10-25 16:16:28 -0700

    tmp

--- run_domino.sh
+++
@@ -1,3 +0,0 @@
-python setup.py install
-
-NTA_LIKELIHOOD_NONLINEAR_FACTOR=$0 python run.py --skipConfirmation --detect --optimize --score --normalize -d numenta -n 2
---
+++ run_experiment.sh
@@ -0,0 +1,3 @@
+python setup.py install
+
+python run.py --skipConfirmation --detect --optimize --score --normalize -d numenta -n 2
